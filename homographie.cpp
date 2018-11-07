e4b6b9d2a2f3a38d8e9d976d0d60d8662cb54b1d
Leon Zheng <leonpapanoel@gmail.com>
2018-11-07 19:44:42 +0800

    Add images and tried StereoBM

--- homographie.cpp
+++ homographie.cpp
@@ -12,8 +12,12 @@ using namespace cv;
 
 int main()
 {
-	Image<uchar> I1 = Image<uchar>(imread("../image1.jpg", CV_LOAD_IMAGE_GRAYSCALE));
-	Image<uchar> I2 = Image<uchar>(imread("../image2.jpg", CV_LOAD_IMAGE_GRAYSCALE));
+//	Image<uchar> I1 = Image<uchar>(imread("../face00.tif", CV_LOAD_IMAGE_GRAYSCALE));
+//	Image<uchar> I2 = Image<uchar>(imread("../face01.tif", CV_LOAD_IMAGE_GRAYSCALE));
+
+    Image<uchar> I1 = Image<uchar>(imread("../perra_7.jpg", CV_LOAD_IMAGE_GRAYSCALE));
+	Image<uchar> I2 = Image<uchar>(imread("../perra_8.jpg", CV_LOAD_IMAGE_GRAYSCALE));
+
 	cout << I1.rows << I1.cols << endl;
 	cout << I2.rows << I2.cols << endl;
 
@@ -44,7 +48,7 @@ int main()
 	// Official doc:Brute-force descriptor matcher.
     //
     //For each descriptor in the first set, this matcher finds the closest descriptor in the second set by trying each one.
-	BFMatcher matcher(NORM_HAMMING);
+	BFMatcher matcher(NORM_L2);
     vector<DMatch>  matches;
     matcher.match(desc1, desc2, matches);
 
@@ -95,7 +99,8 @@ int main()
 //    }
 
     Mat mask;
-    //Mat H = findHomography(keypts1, keypts2, CV_RANSAC, 3, mask);
+    Mat H = findHomography(obj, scene, CV_RANSAC, 3);
+    cout << H << endl;
     Mat F = findFundamentalMat(obj, scene, CV_FM_RANSAC, 3., 0.99, mask);
     cout << F << endl;
     //cout << "Homography matrix" << H << endl;
@@ -115,17 +120,17 @@ int main()
 
     Mat H1, H2;
     stereoRectifyUncalibrated(correct_matches1, correct_matches2, F, I1.size(), H1, H2, 1);
-    Mat R1(2*I1.cols, 2*I1.rows, CV_8U);
-    Mat R2(2*I2.cols, 2*I2.rows, CV_8U);
+    Mat R1(I1.cols, I1.rows, CV_8U);
+    Mat R2(I2.cols, I2.rows, CV_8U);
     warpPerspective(I1, R1, H1, R1.size());
     warpPerspective(I2, R2, H2, R2.size());
     imshow("R1", R1);
     imshow("R2", R2);
 
-//    imwrite("../rectified1.png", R1);
-//    imwrite("../rectified2.png", R2);
-//
-//	// merge two images
+    imwrite("../rectified1.png", R1);
+    imwrite("../rectified2.png", R2);
+
+	// merge two images
 //	Mat K(2 * I1.cols, I1.rows, CV_8U);
 //    Mat idmatrix = Mat::eye(3,3,CV_32F);
 //	warpPerspective(I1, K, idmatrix, Size(2*I1.cols, I1.rows));
@@ -133,21 +138,52 @@ int main()
 //
 //	imshow("merge I1 and I2", K);
 
-    //Interpolation for rectified images
-    Mat A = H2*F*H1.inv();
-    Mat IR(2*I1.cols, 2*I1.rows, CV_8U);
-    double i = 2;
-    for(int y=0; y<R1.rows; y++){
-        for(int x1=0; x1<R1.cols; x1++){
-            Vec3d p1(x1,y,1);
-            Mat p2 = A*Mat(p1);
-            int x2 = int(p2.at<double>(0,0) / p2.at<double>(2,0));
-            int x_i = int((2-i)*x1 + (i-1)*x2);
-            IR.at<uchar>(y,x_i) = (2-i)*R1.at<uchar>(y,x1) + (i-1)*R2.at<uchar>(y,x2);
+
+
+//    Interpolation for rectified images
+//    Mat IR(2*I1.cols, 2*I1.rows, CV_8U);
+//    double i = 2;
+//    for(int y=0; y<R1.rows; y++){
+//        for(int x1=0; x1<R1.cols; x1++){
+//            Vec3d p1(x1,y,1);
+//            Mat p2 = A*Mat(p1);
+//            int x2 = int(p2.at<double>(0,0) / p2.at<double>(2,0));
+//            int x_i = int((2-i)*x1 + (i-1)*x2);
+//            IR.at<uchar>(y,x_i) = (2-i)*R1.at<uchar>(y,x1) + (i-1)*R2.at<uchar>(y,x2);
+//        }
+//    }
+//    imshow("IR", IR);
+//    waitKey(0);
+
+    //Finding disparities
+//    Mat R1 = imread("../chess_1.jpg", CV_LOAD_IMAGE_GRAYSCALE);
+//	Mat R2 = imread("../chess_2.jpg", CV_LOAD_IMAGE_GRAYSCALE);
+
+    Mat disparity;
+
+    Ptr<StereoBM> SBM = StereoBM::create();
+    SBM->compute(R1, R2, disparity);
+
+    int a = 200;
+    int b = 150;
+    for(int i=a-50; i<a+50; i++){
+        for(int j=b-50; j<b+50; j++){
+            cout << disparity.at<short>(j,i) << endl;
         }
     }
-    imshow("IR", IR);
 
+    //Finding corresponding points
+
+	Point t1(a,b);
+	circle(R1, t1, 2, Scalar(0,0,0), 2);
+	imshow("R1", R1);
 	waitKey(0);
+
+	cout << "disparity = " << disparity.at<short>(b, a) << endl;
+	Point t2(a-disparity.at<short>(b, a), b);
+	circle(R2, t2, 2, Scalar(0,0,0), 2);
+	imshow("R2", R2);
+	waitKey(0);
+
 	return 0;
 }
