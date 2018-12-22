0bb27ad8a8942ddc9863f62d56970cc2286aed7c
Leon Zheng <leonpapanoel@gmail.com>
2018-12-22 02:42:47 +0100

    Malloc issue

--- interpolation.h
+++ interpolation.h
@@ -17,7 +17,6 @@
 #include <iostream>
 
 #include "image.h"
-#include "Eigen/src/Core/Matrix.h"
 
 
 struct RectParam{
@@ -36,12 +35,12 @@ public:
     static void matchingKeypoints(const Image<uchar>&I1, const Image<uchar>& I2, int max_good_matches, vector<Point2f>& kptsL, vector<Point2f>& kptsR, bool display=false);
     static void rectify(const Image<uchar>&I1, const Image<uchar>& I2, const vector<Point2f>& kptsL, const vector<Point2f>& kptsR, Image<uchar>& R1, Image<uchar>& R2, RectParam &D);
     static void disparityMapping(const Image<uchar>& R1, const Image<uchar>& R2, Image<short>& disparity);
-    static void interpolate(double i, const Image<uchar>& R1, const Image<uchar>& R2, const Image<short>& disparity, Image<uchar>& IR, RectParam& D);
+    static void interpolate(const Image<uchar>& R1, const Image<uchar>& R2, const Image<short>& disparity, Image<uchar>& IR, RectParam& D);
     static void derectify(const Image<uchar>& IR, const RectParam &D, Image<uchar>& I);
 
 };
 
-inline bool distance_for_matches(DMatch d_i, DMatch d_j) {
+bool distance_for_matches(DMatch d_i, DMatch d_j) {
     return d_i.distance < d_j.distance;
 }
 
